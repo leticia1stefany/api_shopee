e2625050acc89f60652193056f4ea4056ccadad3
dacasfilipe <filiperdacas@gmail.com>
2025-09-04 13:57:18 -0300

    feat: finished entity "Mercadoria"

--- src/main/java/com/shoppee/api/entity/Mercadoria.java
+++ src/main/java/com/shoppee/api/entity/Mercadoria.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @Entity
 public class Mercadoria {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "UUID")
     private UUID id;
     private String nome;
     private double preco;
@@ -18,12 +18,69 @@ public class Mercadoria {
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
+    @ManyToOne
+    @JoinColumn(name = "fornecedor_id")
+    private Fornecedor fornecedor;
 
+    // construtores
+    public Mercadoria() {
+    }
 
-
-
-    // cnstrutores
-
+    public Mercadoria(UUID id, String nome, double preco, int quantidade, Categoria categoria, Fornecedor fornecedor) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+        this.fornecedor = fornecedor;
+    }
     // Getters and Setters
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
 }
