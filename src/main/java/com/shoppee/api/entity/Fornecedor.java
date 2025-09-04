9da4ffe1f49ea872795f0d2689221282c6389fcd
dacasfilipe <filiperdacas@gmail.com>
2025-09-04 14:11:33 -0300

    feat: finished entity "Fornecedor"

---
+++ src/main/java/com/shoppee/api/entity/Fornecedor.java
@@ -0,0 +1,61 @@
+package com.shoppee.api.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "fornecedor")
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY,
+            generator = "UUID")
+    private UUID id;
+    private String nome;
+    private String contato;
+    private String endereco;
+    // construtores
+
+    public Fornecedor() {
+    }
+
+    public Fornecedor(UUID id, String nome, String contato, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.contato = contato;
+        this.endereco = endereco;
+    }
+    //getters e setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
